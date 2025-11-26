4bd73e956e1ae4414adcac2320988e4980082dd8
mknkroth <mknn23g@gmail.com>
2025-11-26 11:08:26 +0700

    Update webhook route /webhook/ninn

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   console.log('----------------------------');
-  console.log('Time:   ', new Date().toISOString());
-  console.log('Method: ', req.method);
-  console.log('URL:    ', req.originalUrl);
-  console.log('Headers:', req.headers);
-  console.log('Body:   ', req.body);
+  console.log('Time:    ', new Date().toISOString());
+  console.log('Method:  ', req.method);
+  console.log('URL:     ', req.originalUrl);
+  console.log('Headers: ', req.headers);
+  console.log('Body:    ', req.body);
   console.log('----------------------------');
   next();
 });
@@ -26,7 +26,7 @@ app.post('/webhook/ninn', (req, res) => {
   res.status(200).send('Webhook received');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Webhook server running on http://localhost:${PORT}`);
 });
